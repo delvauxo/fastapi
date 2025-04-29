2ea33f3acdb1e87e20c539a76208077eee0206a3
delvauxo <delvauxo@outlook.com>
2025-04-29 23:36:21 +0200

    custom_validation_exception_handler() missing argument

--- app/core/exception_handlers.py
+++ app/core/exception_handlers.py
@@ -3,7 +3,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-async def custom_validation_exception_handler(exc: RequestValidationError):
+async def custom_validation_exception_handler(request: Request, exc: RequestValidationError):
     # Vérifier s'il y a une erreur concernant le paramètre 'limit'
     for error in exc.errors():
         if error.get("loc") and error.get("loc")[-1] == "limit":
